6f10b341a836d2b6070b56829164c3e40a4a83f3
Yash Tibrewal <yashtibrewal@Yashs-MacBook-Air.local>
2022-04-21 22:17:23 +0530

    solved 937

---
+++ solved/Solution937.java
@@ -0,0 +1,64 @@
+package solved;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution937 {
+    private class LetterLog implements Comparable<LetterLog> {
+        String identifier;
+        StringBuilder logs;
+
+        LetterLog(String data) {
+            String[] words = data.split(" ");
+            identifier = words[0];
+            logs = new StringBuilder();
+            for(int i=1;i<words.length;i++){
+                logs.append(" "+words[i]);
+            }
+        }
+
+        @Override
+        public int compareTo(LetterLog letterLog) {
+            int result = this.logs.compareTo(letterLog.logs);
+            if(result == 0){
+                return this.identifier.compareTo(letterLog.identifier);
+            }else{
+                return result;
+            }
+        }
+        @Override
+        public String toString(){
+            return identifier + logs.toString();
+        }
+    }
+
+    public String[] reorderLogFiles(String[] logs) {
+        // seperate letter to digital logs
+        ArrayList<LetterLog> letterLogs = new ArrayList<>();
+        ArrayList<String> digitalLogs = new ArrayList<>();
+        String[] words;
+        for (String entry : logs) {
+            words = entry.split(" ");
+            if (words[1].charAt(0) >= '0' && words[1].charAt(0) <= '9') {
+                digitalLogs.add(entry);
+            } else {
+                letterLogs.add(new LetterLog(entry));
+            }
+        }
+        // for letterlogs split them with first word and other words
+        // sort with priority of other words and then the first word, called identifier
+        // here
+        Collections.sort(letterLogs);
+        String[] result = new String[logs.length];
+        int index = 0;
+        for (LetterLog entry : letterLogs) {
+            result[index] = entry.toString();
+            index++;
+        }
+        for (String entry : digitalLogs) {
+            result[index] = entry;
+            index++;
+        }
+        return result;
+    }
+
+}
---
+++ solved/Solution937Test.java
@@ -0,0 +1,21 @@
+package solved;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution937Test{
+
+    static void printString(String[] results){
+        ArrayList<String> printableResults = new ArrayList<>(results.length);
+        Collections.addAll(printableResults, results);
+        System.out.println(printableResults);
+    }
+    public static void main(String[] args){
+        Solution937 solution937 = new Solution937();
+        String[] inputs = new String[]{"dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"};
+        printString(solution937.reorderLogFiles(inputs));
+        inputs = new String[]{"a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"};
+        printString(solution937.reorderLogFiles(inputs));
+        
+    }
+
+}
